b007c5f60595b8304df1ee19ef557324f55f3051
Ben Mendis <bmendis@getwellnetwork.com>
2015-03-17 10:42:38 -0400

    Better output
    -s was redundant with tee
    set priority of stderr to err

--- redirect_output_to_syslog.sh
+++ redirect_output_to_syslog.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-exec 1> >(tee >(logger -s -t $(basename $0)))
-exec 2> >(tee >(logger -s -t $(basename $0)) >&2)
+exec 1> >(tee >(logger -t $(basename $0)))
+exec 2> >(tee >(logger -p err -t $(basename $0)) >&2)
 
 echo "Hello world"
 echo "Goodbye world" >&2
