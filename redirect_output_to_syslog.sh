4e08f05d8486b5a145f8d0c6c48f25c6d7676126
Ben Mendis <bmendis@getwellnetwork.com>
2015-03-17 10:37:46 -0400

    And retain stderr on stderr

--- redirect_output_to_syslog.sh
+++ redirect_output_to_syslog.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-exec 1> >( tee >(logger -s -t $(basename $0))) 2>&1
+exec 1> >(tee >(logger -s -t $(basename $0)))
+exec 2> >(tee >(logger -s -t $(basename $0)) >&2)
 
 echo "Hello world"
+echo "Goodbye world" >&2
 
